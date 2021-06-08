9dafde9ee03e75f3f78f7151a822f7ecb8e1fa49
readdouble <30569947+readdouble@users.noreply.github.com>
2021-06-08 14:16:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World demo\n')
+  response.end('Hello World demo live\n')
 })
 
 server.listen(port)
